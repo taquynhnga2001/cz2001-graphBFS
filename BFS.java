2b4b1d0616b67fd7287882167a4a0661e965c4d3
taquynhnga2001 <taquynhnga2001@gmail.com>
2020-10-14 01:49:44 +0800

    added write output method & record graph structure

--- BFS.java
+++ BFS.java
@@ -1,24 +1,59 @@
-import java.util.LinkedList; 
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.Iterator;
+import java.io.*;
 
 public class BFS {
-    public static Stack<Integer> breathFS(RandomGraph graph, int source) {
-        Queue<Integer> L = new LinkedList<>();   // empty queue L for viz
-        graph.mark(source);  // mark visited vertex source
-        L.add(source);
-        int v=0;  // vertex in queue L
-        int w;  // neighbor of v
-        LinkedList<Integer> neighbors;  // adjacent nodes with v
-        Iterator<Integer> iterator;     // to iterate adjacency linked list
-        int preNode[] = new int[graph.getNodeCount()];  // record pre-incident node (same as Tree)
-        preNode[source] = source;                 // pre-incident node of source node is source node
-        boolean findHos = false;
-        int distance;
-        Stack<Integer> path = new Stack();         // trace path to hospital
+    static int distance;
+    static boolean findHos = false;
 
-        while (L.size()!=0) {
+    public static void main(String[] args) {
+        RandomGraph graph = new RandomGraph(50, 3);
+        graph.markRandHospitals(2);
+
+        // print adjacency list
+        LinkedList<Integer> adjList[] = graph.adjacencyList();
+        LinkedList<Integer> list;
+        Iterator<Integer> iterator;
+        for (int i = 0; i < graph.getNodeCount(); i++) {
+            System.out.print(i + " - ");
+            list = adjList[i];
+            iterator = list.iterator();
+            while (iterator.hasNext())
+                System.out.print(iterator.next() + " ");
+            System.out.println();
+        }
+
+        // write file1: graph structure and file2: hospital Id
+        graph.writeGraphStructure(adjList);
+        graph.writeHospitalFile();
+
+        // algorithm application
+        int sourceVertex = 0;
+        Stack<Integer> path = breathFS(graph, sourceVertex);
+        // clone the path for writing output step
+        Stack<Integer> copiedPath = new Stack<Integer>();   
+        copiedPath.addAll(path);
+
+        graph.display(path, sourceVertex);
+        // write output to a file
+        BFS.writeFile(copiedPath);
+    }
+
+    public static Stack<Integer> breathFS(RandomGraph graph, int source) {
+        Queue<Integer> L = new LinkedList<>();  // empty queue L for viz
+        graph.mark(source); // mark visited vertex source
+        L.add(source);
+        int v = 0;          // vertex in queue L
+        int w;              // neighbor of v
+        LinkedList<Integer> neighbors;      // adjacent nodes with v
+        Iterator<Integer> iterator;         // to iterate adjacency linked list
+        int preNode[] = new int[graph.getNodeCount()]; // record pre-incident node (same as Tree)
+        preNode[source] = source;           // pre-incident node of source node is source node
+        Stack<Integer> path = new Stack();  // trace path to hospital
+
+        while (L.size() != 0) {
             v = L.remove();
             if (graph.isHospital(v)) {
                 findHos = true;
@@ -28,44 +63,50 @@ public class BFS {
             iterator = neighbors.iterator();
             while (iterator.hasNext()) {
                 w = iterator.next();
-                if (graph.mark[w]==0) { // vertex w is unvisited
-                    graph.mark(w);      // mark w as visited
+                if (graph.mark[w] == 0) {   // vertex w is unvisited
+                    graph.mark(w);          // mark w as visited
                     L.add(w);
-                    preNode[w] = v;     // mark the edge vw
+                    preNode[w] = v;         // mark the edge vw
                 }
             }
         }
         if (findHos) {
             distance = 0;
             // then the hospital is v
-            while (preNode[v]!=v) {     // v is not a source node
+            while (preNode[v] != v) { // v is not a source node
                 path.push(v);
-                v = preNode[v];         // v is now its pre-incident node
+                v = preNode[v]; // v is now its pre-incident node
                 distance++;
             }
             System.out.println("The distance is " + distance);
-            path.push(v);               // push the source node
-        } else System.out.println("No way to hospitals!");
+            path.push(v); // push the source node
+        } else
+            System.out.println("No way to hospitals!");
         return path;
     }
 
-    public static void main(String[] args) {
-        RandomGraph graph = new RandomGraph(50, 3);
-        graph.markRandHospitals(2);
-
-        LinkedList<Integer> adjList[] = graph.adjacencyList();
-        LinkedList<Integer> list;
-        Iterator<Integer> iterator;
-        for (int i = 0; i < graph.getNodeCount(); i++) {
-            System.out.print(i + " - ");
-            list = adjList[i]; 
-            iterator = list.iterator();
-            while (iterator.hasNext()) System.out.print(iterator.next() + " ");
-            System.out.println();
+    public static void writeFile(Stack<Integer> path) {
+        try {
+            FileWriter fw = new FileWriter("output.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            
+            if (findHos) {
+                pw.println("The distance is " + distance);
+                int fromNode = path.pop();
+                int toNode;
+                pw.print("BFS Path: " + fromNode);
+                while (!path.empty()) {
+                    toNode = path.pop();
+                    pw.print(" -> " + toNode);
+                    fromNode = toNode;
+                }
+            } else pw.println("No way to hospitals!");
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("IO Error! " + e.getMessage());
+            e.printStackTrace();
+            System.exit(0);
         }
-        int sourceVertex = 0;
-        Stack<Integer> path = breathFS(graph, sourceVertex);
-        graph.display(path, sourceVertex);
-        
     }
 }
--- RandomGraph.java
+++ RandomGraph.java
@@ -5,10 +5,14 @@ import org.graphstream.ui.view.*;
 
 import java.util.LinkedList;
 import java.util.Stack;
+import java.util.Iterator;
 import java.lang.Math;
+
+import java.io.*;
 
 public class RandomGraph {
     private int numNodes;
+    private int numEdges;
     private Graph graph;
     public int[] mark;
     private int[] hospitals;
@@ -23,16 +27,20 @@ public class RandomGraph {
 
         int i;
         for (i = 0; i < numNodes; i++) {
-            // mark[i] = 0;
             gen.nextEvents();
         }
         gen.end();
         numNodes = graph.getNodeCount();
+        numEdges = graph.getEdgeCount();
         mark = new int[numNodes];
     }
 
     public int getNodeCount() {
         return numNodes;
+    }
+
+    public int getEdgeCount() {
+        return numEdges;
     }
 
     public void display(Stack<Integer> path, int sourceVertex) {
@@ -54,15 +62,14 @@ public class RandomGraph {
             while (!path.empty()) {
                 toNode = path.pop();
                 System.out.print(" -> " + toNode);
-                // System.out.println(" getNode: " + graph.getNode(fromNode));
-                edge = graph.getNode(fromNode).getEdgeToward(toNode);
-                // System.out.println(" edge " + edge);
+                edge = graph.getNode(fromNode).getEdgeToward(toNode);   // get edge to fill its color to red
                 edge.setAttribute("ui.class", "path");
                 fromNode = toNode;
             }
             System.out.println();
         }
-        graph.setAttribute("ui.stylesheet", "node.source { fill-color: blue; } node.hos { fill-color: red; }  edge.path { fill-color: red; }");
+        graph.setAttribute("ui.stylesheet",
+                "node.source { fill-color: blue; } node.hos { fill-color: red; }  edge.path { fill-color: red; }");
         graph.display();
     }
 
@@ -70,69 +77,96 @@ public class RandomGraph {
         LinkedList<Integer> adjList[] = new LinkedList[numNodes];
         for (int i = 0; i < numNodes; i++) {
             adjList[i] = new LinkedList<>();
-            // int count = 0;
-            // System.out.print(i + " - ");
             for (int j = 0; j < numNodes; j++) {
                 if (j != i && graph.getNode(i).hasEdgeBetween(j)) {
                     adjList[i].add(j);
-                    // System.out.print(adjList[i].get(count++) + " ");
                 }
-            }
-            // System.out.println();
-        }
+            }        }
         return adjList;
     }
 
     public void mark(int s) {
-        // mark visited vertex
-        mark[s] = 1;
+        mark[s] = 1;        // mark visited vertex
     }
 
-    public void listNodes() {
-        System.out.println("List of nodes Id: ");
-        for (Node n : graph) {
-            System.out.print(n.getId() + " ");
-        }
-        System.out.println();
-    }
-    public void getEdge(int i, int j) {
-        if (graph.getNode(i).hasEdgeBetween(j)) {
-            Edge e = graph.getNode(i).getEdgeToward(j);
-            System.out.print(" Edge: "+e+"; ");
-        }
-    }
     public void markRandHospitals(int numHos) {
+        // param: numHos = number of random hospital id you want to generate
         hospitals = new int[numHos];
-        for (int i=0; i<numHos; i++) hospitals[i] = graph.getNodeCount();     // initialize all hosIDs to be different from 0, and we choose = numNodes
-
+        for (int i = 0; i < numHos; i++)
+            hospitals[i] = graph.getNodeCount(); // initialize all hosIDs to be different from 0, and we choose =
+                                                 // numNodes
         int hosId;
         Node node;
-        boolean loopMore=false;
+        boolean loopMore = false;
         System.out.print("Hospital: ");
 
-        for (int i=0; i<numHos; i++){
-            do {    // generate random hosId
-                hosId = (int)(Math.random() * (graph.getNodeCount()));
-                for (int h: hospitals) {
-                    if (hosId==h) {
+        for (int i = 0; i < numHos; i++) {
+            do { // generate random hosId
+                hosId = (int) (Math.random() * (graph.getNodeCount()));
+                for (int h : hospitals) {
+                    if (hosId == h) {
                         loopMore = true;
                         break;
-                    } else loopMore = false;
+                    } else
+                        loopMore = false;
                 }
-            } while (loopMore);  // generate another hosID if hospitals contain that hosId
+            } while (loopMore); // generate another hosID if hospitals contain that hosId
 
             hospitals[i] = hosId;
-            System.out.print(hosId+"\t");
+            System.out.print(hosId + "\t");
             node = graph.getNode(hosId);
             System.setProperty("org.graphstream.ui", "swing");
-            node.setAttribute("ui.class", "hos");   // add CSS class attribute for further display
+            node.setAttribute("ui.class", "hos"); // add CSS class attribute for further display
         }
         System.out.println();
     }
+
     public boolean isHospital(int id) {
-        for (int hosId: hospitals) {
-            if (id == hosId) return true;
+        for (int hosId : hospitals) {
+            if (id == hosId)
+                return true;
         }
         return false;
+    }
+
+    public void writeGraphStructure(LinkedList<Integer> adjList[]) {
+        try {
+            FileWriter fw = new FileWriter("file1.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            pw.println("# Undirected graph: file1.txt");
+            pw.println("# Random graph generator");
+            pw.println("# Nodes: " + numNodes + " Edges: " + numEdges);
+            pw.println("# FromNodeId\tToNodeId");
+            for (int i = 0; i < numNodes; i++) {
+                LinkedList<Integer> list;
+                Iterator<Integer> iterator;
+                list = adjList[i];
+                iterator = list.iterator();
+                while (iterator.hasNext())
+                    pw.println(i + "\t" + iterator.next());
+            }
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("IO Error! " + e.getMessage());
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public void writeHospitalFile () {
+        try {
+            FileWriter fw = new FileWriter("file2.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println("# " + hospitals.length);
+            for (int hosId: hospitals) pw.println(hosId);
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("IO Error! " + e.getMessage());
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 }
